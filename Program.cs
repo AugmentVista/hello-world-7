552160ba35ecb8852524f3624aa66155cba8dd4a
AugmentVista <AuAgments@gmail.com>
2022-09-12 12:27:32 -0300

    world 7

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("By Silver ");
+            Console.WriteLine("Attempt 5 ");
+            Console.ReadKey(true);
+
+
+
+        }
+    }
+}
